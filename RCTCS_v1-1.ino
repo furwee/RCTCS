9cabf7b07dcd322ed472ee9de45e547cfd2e098a
furwee <152007283+furwee@users.noreply.github.com>
2026-01-19 02:38:38 +0800

    Update RCTCS_v1-2.ino
    PROGMEM added
    
    increased data rate and IIC speed at the cost of rounding sensor values
    
    added a velocity reset with a lower end of ESC pwm input

--- RCTCS_v1-1.ino
+++ RCTCS_v1-1.ino
@@ -5,6 +5,7 @@ SERVO AND ESC USE uS (microseconds) as unit
 */
 
 // modules to include
+#include <avr/pgmspace.h>
 #include <Adafruit_LSM6DS33.h>  // IMU
 #include <Wire.h>               // IIC
 #include "PWM.hpp"              // INPUT https://github.com/xkam1x/Arduino-PWM-Reader
@@ -25,8 +26,8 @@ bool toggle = LOW;
 Adafruit_LSM6DS33 IMU;
 
 // gyro offset, set 0 if calibrate at start, hardcode if stable
-float IMU_OFFSET_X = -3.85;  // acx, + is forward, - is backward
-float IMU_OFFSET_Y = -28.8;  // acy, + is right and - is left (acceleration)
+float IMU_OFFSET_X = -3.6;  // acx, + is forward, - is backward
+float IMU_OFFSET_Y = -29;  // acy, + is right and - is left (acceleration)
 float IMU_OFFSET_Z = 0;  // gyz, + is left and - is right (rotation)
 
 // IMU calibrated datas
@@ -49,19 +50,26 @@ float vel; // velocity
 int servoCorrectionSteps = 50; // value between 1-500
 int relativeServoCorrectionSteps;
 int throttleCorrectionSteps = 100; // value between 1-500
-float counterSteerStrength = 1.5;
-float alpha = 0.005;
-float tolerance = 0.2;
-float velThreshold = 6;
 
+static float accFilter = 0.0f; // LPF accXY
+
+// 0: counterSteerStrength, 1: alpha, 2: tolerance, 3: velThreshold, 4: filterAlpha
+const PROGMEM float variablesFLOAT[5] = {1.5f, 0.005f, 0.2f, 6.0f, 0.15};
 
 void setup() {
-    while (!IMU.begin_I2C()){
+  // Serial.begin(9600);
+  while (!IMU.begin_I2C()){
     delay(5000);
   }
+
+  Wire.setClock(400000); // so fast :O
+
   // IMU setup
   IMU.setGyroRange(LSM6DS_GYRO_RANGE_1000_DPS);
   IMU.setAccelRange(LSM6DS_ACCEL_RANGE_8_G);
+  IMU.setAccelDataRate(LSM6DS_RATE_3_33K_HZ);
+  IMU.setGyroDataRate(LSM6DS_RATE_3_33K_HZ);
+
 
   /*pin section*/
   pinMode(RXLED, OUTPUT);  // Set RX LED as an output
@@ -76,39 +84,51 @@ void setup() {
 }
 
 void loop() {
-  TXLED0;
+  
+  TXLED0; // just shutting the led up, dont mind it
+
   // gyro event
   sensors_event_t accel;
   sensors_event_t gyro;
   sensors_event_t temp;
   IMU.getEvent(&accel, &gyro, &temp);
 
-  //offset to get calibrated data
-  acX = accel.acceleration.x + IMU_OFFSET_X;
-  acY = accel.acceleration.y + IMU_OFFSET_Y;
+  //offset to get calibrated data, rounded off to filter noise from 3.33k data rate
+  acX = round((accel.acceleration.x + IMU_OFFSET_X) *0.1)*10;
+  acY = round((accel.acceleration.y + IMU_OFFSET_Y) *0.1)*10;
   gyZ = gyro.gyro.z + IMU_OFFSET_Z; // refer to ln 25-27
+  /* Serial.println(acX);
+  Serial.println(acY);
+  Serial.println(gyZ);
+  Serial.println(); */
 
   // reading input from jetson nano
   int servo = constrain(servoIN.getValue(), 1000, 2000);
-  int ESC = constrain(ESCIN.getValue(), 1500, 2000);
+  int ESC = constrain(ESCIN.getValue(), 1450, 2000);
 
   //algorithm(s)
 
   // generate expected yaw rate
   float accXY = sqrt(acX*acX + acY*acY); // get resultant velocity
+  accFilter = pgm_read_float(&variablesFLOAT[4])  * accXY + (1.0f - pgm_read_float(&variablesFLOAT[4])) * accFilter;
 
   // sample velocity
   if (millis() - lastSampleTime > 10) {
-    vel += accXY * 0.01; // v = at
+    vel += accFilter * 0.01; // v = at
     lastSampleTime = millis();
+  }
+
+  //reset velocity
+  if (ESC < 1500) {
+    vel = 0.0f;
   }
 
   // clock led
   // digitalWrite(RXLED, millis()%500>10); // https://www.reddit.com/r/arduino/comments/n1suiz/led_blink_no_delay_one_line_of_code/ <- VERY USEFUL TO TELL IF ITS WORKING
 
   // converted from https://www.desmos.com/calculator/n14dt2nzrk
-  float kvel = 1/(1- alpha * vel); // ln 53
-  float expectedYawRate = floor(constrain(500 - 490 * cos(3.14f * (servo - 1500) * 0.002f * kvel), 0, 1000)/25)*25;
+  float kvel = 1/(1- pgm_read_float(&variablesFLOAT[1]) * vel); // ln 53
+  float expectedYawRate = floor(constrain(500 - 490 * cos(0.00628 * (servo - 1500) * kvel), 0, 1000)* 0.04f)*25;
   
   //direction
   int sign = (servo < 1450) ? -1 : (servo > 1550) ? 1 : 0; // set positive or negative or a 0 if deadzone (100uS)
@@ -116,19 +136,19 @@ void loop() {
 
   //correction
   float diff = gyZ - expectedYawRate;
-  if (diff < -tolerance) { // refer to ln 54
+  if (diff < -1 * pgm_read_float(&variablesFLOAT[2])) { // refer to ln 54
     //understeer, reduce throttle and add steering
     servo += relativeServoCorrectionSteps; 
 
-    ESC = (vel >= velThreshold) ? ESC -= throttleCorrectionSteps : ESC += throttleCorrectionSteps; // refer to ln 51, handles both high speed understeer olr low speed understeer
+    ESC = (vel >= pgm_read_float(&variablesFLOAT[3])) ? (ESC - throttleCorrectionSteps) : (ESC + throttleCorrectionSteps); // refer to ln 51, handles both high speed understeer olr low speed understeer
   
-  } else if (diff > tolerance) {
+  } else if (diff > pgm_read_float(&variablesFLOAT[2])) {
     // oversteer, counter-steer and maintain throttle until it is normal again
-    servo -= counterSteerStrength * relativeServoCorrectionSteps;
+    servo -= pgm_read_float(&variablesFLOAT[0]) * relativeServoCorrectionSteps;
     ESC -= throttleCorrectionSteps * 0.2f;
   }
 
   // output
   servoOUT.writeMicroseconds(constrain(servo, 1000, 2000));
-  ESCOUT.writeMicroseconds(constrain(ESC, 1500, 2000));  
+  ESCOUT.writeMicroseconds(constrain(ESC, 1450, 2000)); 
 }
